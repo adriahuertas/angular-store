e44cb30fa230b1170c769dba8bce245b1fd54231
Adria Huertas <adriahc@gmail.com>
2023-10-12 14:27:20 +0200

    Added Cart Service/Logic

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core"
+import { BehaviorSubject } from "rxjs"
+import { Cart, CartItem } from "../models/cart.model"
+import { MatSnackBar } from "@angular/material/snack-bar"
+
+@Injectable({
+  providedIn: "root",
+})
+export class CartService {
+  cart = new BehaviorSubject<Cart>({ items: [] })
+  constructor(private _snackBar: MatSnackBar) {}
+
+  addToCart(item: CartItem): void {
+    const items = [...this.cart.value.items]
+    const index = items.findIndex((i) => i.id === item.id)
+    if (index === -1) {
+      items.push(item)
+    } else {
+      items[index].quantity += 1
+    }
+
+    this.cart.next({ items })
+    this._snackBar.open("Product added to cart", "Ok", {
+      duration: 3000,
+    })
+    console.log(this.cart.value)
+  }
+
+  getTotal(items: CartItem[]): number {
+    return items.reduce((acc, item) => acc + item.quantity * item.price, 0)
+  }
+
+  clearCart(): void {
+    this.cart.next({ items: [] })
+    this._snackBar.open("Cart cleared", "Ok", {
+      duration: 3000,
+    })
+  }
+}
